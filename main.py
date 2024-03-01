b8fe5abeae763359fbac079b024270dbf11df7a3
gruand <gruand@gmail.com>
2024-03-01 12:29:10 +0200

    Add endpoints for all data by station, and annual data

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ app = Flask(__name__)
 
 stations = pd.read_csv("data/stations.txt", skiprows=17)
 stations = stations[["STAID", "STANAME                                 "]][:16].to_html()
+
+
 @app.route("/")
 # Decorator
 def home():
@@ -26,6 +28,33 @@ def get_temperature(station, date):
         return {"station": "No Such Station",
                 "date": date,
                 "temperature": "N/A"}
+
+
+@app.route("/api/v1/<station>")
+def all_data(station):
+    try:
+        file_name = f"data/TG_STAID{str(station).zfill(6)}.txt"
+        df = pd.read_csv(file_name, skiprows=20, parse_dates=["    DATE"])
+        result = df.to_dict(orient="records")
+        return result
+    except FileNotFoundError:
+        return "No such station"
+
+
+@app.route("/api/v1/annual/<station>/<year>")
+def annual(station, year):
+    try:
+        file_name = f"data/TG_STAID{str(station).zfill(6)}.txt"
+        df = pd.read_csv(file_name, skiprows=20)
+        # Convert date to string
+        df["    DATE"] = df["    DATE"].astype(str)
+        # Filter by years starting with needed
+        result = df[df["    DATE"].str.startswith(str(year))]
+        result = result.to_dict(orient="records")
+        return result
+    except FileNotFoundError:
+        return "No such station"
+
 
 if __name__ == "__main__":
     app.run(debug=True)
